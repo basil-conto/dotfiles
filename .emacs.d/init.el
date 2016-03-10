9ff585ee4562e666e0cfe8f519049ac28d186d60
Basil L. Contovounesios <contovob@tcd.ie>
2016-03-10 11:44:32 +0000

    [EMACS] Install annoying-arrows-mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -186,6 +186,11 @@ instead of the current point, i.e. the region defined by `mark-paragraph`."
   (setq ag-highlight-search t)
   (add-to-list 'ag-arguments "-C 5"))
 
+(use-package annoying-arrows-mode
+  :ensure t
+  :config
+  (global-annoying-arrows-mode))
+
 (use-package tex
   :ensure auctex
   :config
