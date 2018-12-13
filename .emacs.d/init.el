c56a105ec26ff54176e498dc75b1f3acffa3ffb1
Basil L. Contovounesios <contovob@tcd.ie>
2018-12-13 13:48:15 +0000

    [EMACS] Add forgotten terminal buffer base name

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1727,6 +1727,7 @@ ${author:30} ${date:4} ${title:*} ${=has-pdf=:1}${=has-note=:1} ${=type=:14}"))
  sx-question-mode-comments-format       "%s:\n   %s\n"
 
  ;; term
+ term-ansi-buffer-base-name             "term"
  term-buffer-maximum-size               4096
  term-suppress-hard-newline             t
 
