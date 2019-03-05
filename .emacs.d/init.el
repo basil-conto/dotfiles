1e4223782cfae52876b8e9fe865400aef7f62a04
Basil L. Contovounesios <contovob@tcd.ie>
2019-03-05 00:11:42 +0000

    [EMACS] Ignore trailing whitespace in comint

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1869,6 +1869,7 @@ ${author:30} ${date:4} ${title:*} ${=has-pdf=:1}${=has-note=:1} ${=type=:14}"))
   ;; blc-lib
   (:fns blc-turn-off-trailing-whitespace :hooks (Info-mode-hook
                                                  calendar-mode-hook
+                                                 comint-mode-hook
                                                  eww-buffers-mode-hook
                                                  eww-mode-hook
                                                  help-mode-hook
