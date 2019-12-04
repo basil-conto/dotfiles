a4213c342ee14f24a41a1b5dbe1ae473b2bbc91f
Basil L. Contovounesios <contovob@tcd.ie>
2019-12-04 11:12:24 +0000

    [EMACS] Fix yet another logview-mode-hook typo

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1904,7 +1904,7 @@ ${author:30} ${date:4} ${title:*} ${=has-pdf=:1}${=has-note=:1} ${=type=:14}"))
                                                  eww-buffers-mode-hook
                                                  eww-mode-hook
                                                  help-mode-hook
-                                                 log-view-mode-hook
+                                                 logview-mode-hook
                                                  message-mode-hook
                                                  messages-buffer-mode-hook
                                                  minibuffer-setup-hook
