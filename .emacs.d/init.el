c1d37af586d8d6295b08785e8ebb762006d6ecc0
Basil L. Contovounesios <contovob@tcd.ie>
2020-10-19 18:45:59 +0100

    [EMACS] Fix setting of emoji font in Emacs daemon

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -374,6 +374,14 @@ Offer all entities found in `org-entities-user' and
 
 ;;;; DEFINITIONS
 
+;; fontset.c
+
+(defun blc-turn-on-emoji-font (_frame)
+  "Add an emoji font to the default fontset.
+Do this only once in the first non-daemon initial frame."
+  (set-fontset-font t 'symbol "Noto Color Emoji" nil 'append)
+  (remove-hook 'after-make-frame-functions #'blc-turn-on-emoji-font))
+
 ;; auctex
 
 (defun blc-LaTeX-command-default ()
@@ -1875,6 +1883,9 @@ ${author:30} ${date:4} ${title:*} ${=has-pdf=:1}${=has-note=:1} ${=type=:14}"))
 ;;;; HOOKS
 
 (blc-hook
+  ;; fontset.c
+  (:hooks after-make-frame-functions :fns blc-turn-on-emoji-font)
+
   ;; auctex
   (:hooks TeX-after-compilation-finished-functions
           :fns TeX-revert-document-buffer)
@@ -2198,10 +2209,6 @@ ${author:30} ${date:4} ${title:*} ${=has-pdf=:1}${=has-note=:1} ${=type=:14}"))
 
 
 ;;;; PACKAGES
-
-;; fontset.c
-
-(set-fontset-font t 'symbol "Noto Color Emoji" nil 'append)
 
 ;; ace-window
 
