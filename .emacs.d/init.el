b3a568f71c39e3307c8b8df6b79af72e480e7742
Basil L. Contovounesios <contovob@tcd.ie>
2021-03-17 10:00:30 +0000

    [EMACS] Shorten Savannah URL

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2883,7 +2883,7 @@ ${author:30} ${date:4} ${title:*} ${=has-pdf=:1}${=has-note=:1} ${=type=:14}"))
          "\
 [%N]: %s
 %h %ci
-https://git.savannah.gnu.org/cgit/emacs.git/commit/?id=%H\n"
+https://git.sv.gnu.org/cgit/emacs.git/commit/?id=%H\n"
          (caddr magit-pop-revision-stack-format))))
 
 ;; magit-log
