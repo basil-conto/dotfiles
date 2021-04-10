2527626e899049181b88a2e584bcdc59e901ce21
Basil L. Contovounesios <contovob@tcd.ie>
2021-04-11 00:06:17 +0100

    [EMACS] Remove redundant xref setting

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1912,7 +1912,6 @@ ${author:30} ${date:4} ${title:*} ${=has-pdf=:1}${=has-note=:1} ${=type=:14}"))
  split-window-keep-point                nil
 
  ;; xref
- xref-file-name-display                 'project-relative
  xref-search-program                    'ripgrep
 
  ;; youtube-dl
