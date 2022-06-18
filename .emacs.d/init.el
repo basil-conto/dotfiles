b31b30abd641b4721a1df76493ed45b5242a0fd3
Basil L. Contovounesios <contovob@tcd.ie>
2022-06-18 16:14:51 +0300

    [EMACS] Use new lines-char long line highlighting

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1967,7 +1967,7 @@ ${author:30} ${date:4} ${title:*} ${=has-pdf=:1}${=has-note=:1} ${=type=:14}"))
        #'shell-mode
        #'wdired-mode)
  whitespace-style
- '(face lines-tail missing-newline-at-eof tab-mark)
+ '(face lines-char missing-newline-at-eof tab-mark)
 
  ;; wid-edit
  widget-menu-minibuffer-flag            t
